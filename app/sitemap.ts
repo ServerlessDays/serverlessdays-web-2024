016bd144df346a3c2f146f5065ac96f5718c488a
Simone Lusenti <lusenti.s@gmail.com>
2024-10-07 17:00:59 +0200

    add sitemap
    Signed-off-by: Simone Lusenti <lusenti.s@gmail.com>

---
+++ app/sitemap.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from "next";
+import { getEvents } from "@/lib/server";
+
+const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "https://www.serverlessdays.io";
+const lastModified = new Date(); // cached at build time
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const conferences = await getEvents();
+
+  const conferenceUrls = conferences.map((conference) => ({
+    url: `${BASE_URL}/${conference.slug}`,
+    lastModified: lastModified,
+  }));
+
+  return [
+    {
+      url: `${BASE_URL}`,
+      lastModified: lastModified,
+    },
+    {
+      url: `${BASE_URL}/coc`,
+      lastModified: lastModified,
+    },
+    {
+      url: `${BASE_URL}/past-events`,
+      lastModified: lastModified,
+    },
+    ...conferenceUrls,
+  ];
+}
