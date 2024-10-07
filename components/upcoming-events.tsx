d6511e694756a44fe74ba993deb2c87bad648983
Simone Lusenti <lusenti.s@gmail.com>
2024-10-07 17:01:51 +0200

    cleanup
    Signed-off-by: Simone Lusenti <lusenti.s@gmail.com>

--- components/upcoming-events.tsx
+++ components/upcoming-events.tsx
@@ -12,7 +12,7 @@ export async function UpcomingEvents() {
         Upcoming conferences
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {events.map((event, ii) => (
+        {events.map((event) => (
           <UpcomingEventCard key={event.slug} event={event} />
         ))}
       </div>
